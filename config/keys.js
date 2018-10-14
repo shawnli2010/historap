7470a9c9ccb795c1fd4de8e48a5e7f94dc9e47ab
Shawn Li <shawnli2010@126.com>
2018-10-13 23:28:10 -0700

    tested database connection and created a document

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://shawnli2010:1314520@cluster0-shard-00-00-7uazm.mongodb.net:27017,cluster0-shard-00-01-7uazm.mongodb.net:27017,cluster0-shard-00-02-7uazm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+    "mongodb://shawnli2010:1314520@cluster0-shard-00-00-7uazm.mongodb.net:27017,cluster0-shard-00-01-7uazm.mongodb.net:27017,cluster0-shard-00-02-7uazm.mongodb.net:27017/anotherTest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "secret"
 };
--- routes/api/historyEvent.js
+++ routes/api/historyEvent.js
@@ -12,4 +12,31 @@ const Coordinate = require("../../models/Coordinate");
 //@access   Public
 router.get("/test", (req, res) => res.json({ msg: "HistoryEvent Works" }));
 
+//@route    POST api/historyEvent/test
+//@desc     Test database connection
+//@access   Public
+router.post("/test", (req, res) => {
+  const testCoordinate = new Coordinate({
+    latitude: 20,
+    longitude: 30
+  });
+
+  const testEvent = new HistoryEvent({
+    name: "huang jin qi yi",
+    coordinate: testCoordinate,
+    description: "this is a test history event",
+    unRelatedField: 100
+  });
+
+  testCoordinate
+    .save()
+    .then(coordinate => {
+      testEvent
+        .save()
+        .then(event => res.json(event))
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
 module.exports = router;
